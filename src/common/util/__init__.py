173c5f501bae0e84cb308bf2283e25bc1c7ee991
Madalievnkx <146520663+Madalievnkx@users.noreply.github.com>
2023-12-09 20:10:01 +0100

    update file

---
+++ src/common/util/__init__.py
@@ -0,0 +1,11 @@
+"""
+common > util
+
+Contains useful helper functions for simplifying code in other components
+
+Authors:
+* Miguel Guthridge [hdsq@outlook.com.au, HDSQ#2154]
+
+This code is licensed under the GPL v3 license. Refer to the LICENSE file for
+more details.
+"""
