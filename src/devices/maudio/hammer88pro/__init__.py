fb1c280ddd7a69522e35786cc4a11ec8714bbae6
Madalievnkx <146520663+Madalievnkx@users.noreply.github.com>
2023-12-09 20:10:54 +0100

    update file

---
+++ src/devices/maudio/hammer88pro/__init__.py
@@ -0,0 +1,16 @@
+"""
+devices > maudio > hammer88pro
+
+M-Audio Hammer 88 Pro
+
+Authors:
+* Miguel Guthridge [hdsq@outlook.com.au, HDSQ#2154]
+
+This code is licensed under the GPL v3 license. Refer to the LICENSE file for
+more details.
+"""
+__all__ = [
+    'hammer88pro'
+]
+
+from . import hammer88pro
