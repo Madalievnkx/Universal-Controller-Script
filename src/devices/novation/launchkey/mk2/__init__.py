6baa8e78c62ff7d0a85b2c1071b5eea8b22c7b35
Madalievnkx <146520663+Madalievnkx@users.noreply.github.com>
2023-12-09 20:11:05 +0100

    update file

---
+++ src/devices/novation/launchkey/mk2/__init__.py
@@ -0,0 +1,18 @@
+"""
+devices > novation > launchkey > mk2
+
+Device definitions for Launchkey Mk2 series
+
+Authors:
+* Miguel Guthridge [hdsq@outlook.com.au, HDSQ#2154]
+
+This code is licensed under the GPL v3 license. Refer to the LICENSE file for
+more details.
+"""
+
+__all__ = [
+    'LaunchkeyMk2_25',
+    'LaunchkeyMk2_49_61',
+]
+
+from .launchkey import LaunchkeyMk2_25, LaunchkeyMk2_49_61
